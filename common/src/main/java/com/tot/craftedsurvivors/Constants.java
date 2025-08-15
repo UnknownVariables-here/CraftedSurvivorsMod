dfb1a81f1a2280600f19a1d49dd57c82bb20dafa
Tot536 <rochowkilian@gmail.com>
2025-08-15 22:25:47 +0200

    u stinky

---
+++ common/src/main/java/com/tot/craftedsurvivors/Constants.java
@@ -0,0 +1,11 @@
+package com.tot.craftedsurvivors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "craftedsurvivors";
+	public static final String MOD_NAME = "CraftedSurvivors";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
